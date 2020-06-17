1b0f1b82d6d9b3a78ec3a9517324c44c65efd2a6
sini0124 <siniim0124@gmail.com>
2020-06-17 12:20:03 +0900

    Initial Commit

---
+++ mask_24/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  mask_24
+//
+//  Created by 시니 on 2020/06/17.
+//  Copyright © 2020 Data Science Dept. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
